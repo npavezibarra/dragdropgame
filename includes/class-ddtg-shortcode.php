1be6573beba52dfe4caf4bd88ecddf7f60fa7af4
Nicolás Pavez <63881641+npavezibarra@users.noreply.github.com>
2025-11-16 11:35:32 -0300

    Add dragdropgame shortcode with legacy support and validation

--- includes/class-ddtg-shortcode.php
+++ includes/class-ddtg-shortcode.php
@@ -18,7 +18,26 @@ class DDTG_Shortcode {
      * Initialize the shortcode.
      */
     public static function init() {
-        add_shortcode( 'draglearn_game', array( __CLASS__, 'render_game' ) );
+        add_shortcode( 'dragdropgame', array( __CLASS__, 'render_game' ) );
+        add_shortcode( 'draglearn_game', array( __CLASS__, 'render_legacy_game' ) );
+    }
+
+    /**
+     * Legacy shortcode handler for backward compatibility.
+     *
+     * @param array  $atts    Shortcode attributes.
+     * @param string $content Shortcode content.
+     * @param string $tag     Shortcode tag.
+     * @return string
+     */
+    public static function render_legacy_game( $atts, $content = null, $tag = 'draglearn_game' ) {
+        _doing_it_wrong(
+            'draglearn_game',
+            esc_html__( 'The [draglearn_game] shortcode is deprecated. Please use [dragdropgame] instead.', 'draglearndtg' ),
+            '1.6.0'
+        );
+
+        return self::render_game( $atts, $content, $tag );
     }
 
     /**
@@ -27,15 +46,19 @@ class DDTG_Shortcode {
      * @param array $atts Shortcode attributes.
      * @return string
      */
-    public static function render_game( $atts ) {
-        $atts = shortcode_atts( array(
-            'game' => '',
-        ), $atts, 'draglearn_game' );
+    public static function render_game( $atts, $content = null, $tag = 'dragdropgame' ) {
+        $atts = shortcode_atts(
+            array(
+                'game' => '',
+            ),
+            $atts,
+            $tag
+        );
 
-        $shortcode_slug = sanitize_text_field( $atts['game'] );
+        $shortcode_slug = sanitize_title( $atts['game'] );
 
         if ( empty( $shortcode_slug ) ) {
-            return '<p>' . esc_html__( 'Game slug is missing.', 'draglearndtg' ) . '</p>';
+            return '<p>' . esc_html__( 'A valid game slug is required.', 'draglearndtg' ) . '</p>';
         }
 
         if ( ! is_user_logged_in() ) {
